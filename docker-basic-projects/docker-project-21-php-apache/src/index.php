072a2fa5d512f38598a5cc060b9ef6a851caba3f
ashutosh8rai <ashutosh2k50@gmail.com>
2026-08-10 22:15:04 +0530

    Project 21: Containerize a PHP Apache web application

---
+++ docker-basic-projects/docker-project-21-php-apache/src/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Docker Project 21</title>
+</head>
+<body>
+
+<h1>Docker Project 21</h1>
+
+<h2>Hello from PHP + Apache!</h2>
+
+<p>Current Server Time:
+<?php
+echo date("Y-m-d H:i:s");
+?>
+</p>
+
+</body>
+</html>
